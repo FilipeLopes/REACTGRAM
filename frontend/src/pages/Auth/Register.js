74e2e5a918cbaa0e3b3e2294d3926bc7cc19b2aa
Filipe Lopes <lopes.filiper@gmail.com>
2023-02-21 17:17:26 +0100

    Creating register form

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,8 +1,32 @@
 import "./Auth.css";
 
+// Components
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useState, useEffect } from "react";
+
 const Register = () => {
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Register to see your friends photos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name"/>
+        <input type="email" placeholder="Email"/>
+        <input type="password" placeholder="Password" />  
+        <input type="password" placeholder="Confirm password" />  
+        <input type="submit" value="Register" />
+      </form>
+      <p>
+        Do you already have an account? <Link to="/login">Click here</Link>
+      </p>
+    </div>
   )
 }
 
